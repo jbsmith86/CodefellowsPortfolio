73dbc5a2aea1228697fd1e64268e8ca71974768e
Joel Smith <joely5525@gmail.com>
2014-01-17 15:22:29 -0800

    fixed zerb test

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class CheckForZurbTest < Capybara::Rails::TestCase
   feature "Using Zerb for styling" do
     scenario "check to see if zerb is loaded" do
-      visit root_path
-      puts page.all
+      visit posts_path
+      assert page.body.must_include "columns"
     end
   end
 end
