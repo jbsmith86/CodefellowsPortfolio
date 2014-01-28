0dcd46fa9dfe2b8e22851669270b6b14a050c80c
Joel Smith <joely5525@gmail.com>
2014-01-27 17:49:42 -0800

    Everything before Pundit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,10 +15,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  def sign_in
+  def sign_in(role = :editor)
     visit new_user_session_path
-    fill_in "Email", with: users(:starsky).email
-    fill_in "Password", with: "1234"
+    fill_in "Email", with: users(role).email
+    fill_in "Password", with: "password"
     click_on "Sign in"
   end
 end
